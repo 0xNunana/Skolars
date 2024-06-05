bf27e2f0d268087aad20966ab90c98dd21767bae
0xNunana <kudayapaul@gmail.com>
2024-06-05 15:04:45 +0000

    added contact page

---
+++ app/(pages)/Services/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='min-h-screen'>
+        <div className='max-w-screen-2xl px-5 mx-auto'>
+            page
+        </div>
+    </div>
+  )
+}
+
+export default page
